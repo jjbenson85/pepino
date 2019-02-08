31062bac89aeb44746a1d02c4fbc774e44e25c04
Begona Fernandez <boadicea1979@yahoo.es>
2019-02-08 13:53:25 +0000

    Update project route

--- models/project.js
+++ models/project.js
@@ -11,11 +11,19 @@ const projectSchema = new mongoose.Schema({
   name: { type: String, required: 'You must provide a project name' },
   description: { type: String },
   user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
-  packages: { type: mongoose.Schema.ObjectId, ref: 'Package'},
+  packages: [{ type: mongoose.Schema.ObjectId, ref: 'Package'}],
   public: { type: Boolean, required: true, default: true},
   comments: [ commentSchema ]
 }, {
   timestamps: true
+})
+
+
+projectSchema.set('toJSON', {
+  transform(doc, json) {
+    delete json.__v
+    return json
+  }
 })
 
 module.exports = mongoose.model('Project', projectSchema)
