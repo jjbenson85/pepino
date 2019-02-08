413bb0ab93fb1db0854a0bdf01a0f6b11564f7d9
Begona Fernandez <boadicea1979@yahoo.es>
2019-02-08 09:44:30 +0000

    Initial Index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+mongoose.plugin(require('mongoose-unique-validator'))
+const bodyParser = require('body-parser')
+const errorHandler = require('./lib/errorHandler')
+
+const app = express()
+
+const routes = require('./config/routes')
+
+mongoose.connect(process.env.MONGODB_URI)
+
+app.use(express.static(`${__dirname}/public`))
+
+app.use(bodyParser.json())
+app.use('/api', routes)
+app.use('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`))
+
+app.use(errorHandler)
+
+app.listen(process.env.PORT, () => console.log(`Up and running on port ${process.env.PORT}`))
+
+module.exports = app
