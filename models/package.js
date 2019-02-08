6d1f01110d12a2b057e25a6190cc569631183773
James Benson <you@example.com>
2019-02-08 14:09:11 +0000

    Added user to seeds.js, added comments to packages

--- models/package.js
+++ models/package.js
@@ -1,26 +1,30 @@
 const mongoose = require('mongoose')
+const User = require('../models/user')
 
 //Add comments as feature later
-// const commentSchema = new mongoose.Schema({
-//   text: { type: String, required: true },
-//   user: { type: mongoose.Schema.ObjectId, ref: 'Author', required: true }
-// })
+const commentSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.ObjectId, ref: 'User'},
+  text: { type: String, required: true }
+},{
+  timeStamps: true
+})
 
 const packageSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   description: { type: String, required: true },
   keywords: [{ type: String}],
-  downloadsCount: { type: Number}
-  // comments: [commentSchema]
+  downloadsCount: { type: Number},
+  comments: [commentSchema]
 })
 
 
-packageSchema.set('toJSON', {
-  virtuals: true,
-  transform(doc, json) {
-    delete json.__v
-    return json
-  }
-})
+
+// packageSchema.set('toJSON', {
+//   virtuals: true,
+//   transform(doc, json) {
+//     delete json.__v
+//     return json
+//   }
+// })
 
 module.exports = mongoose.model('Package', packageSchema)
---
+++ src/app.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Header from './components/common/header'
+import PackageShow from './components/packages/packageShow'
+// import BooksIndex from './components/books/BooksIndex'
+// import Home from './components/Home'
+
+
+import './scss/style.scss'
+
+class App extends React.Component {
+
+  render(){
+    return(
+      <BrowserRouter>
+        <main>
+          <Header />
+          <Switch>
+            <Route path="/packages/:name" component={PackageShow} />
+          </Switch>
+        </main>
+      </BrowserRouter>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
