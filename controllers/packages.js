53cf0cb96a18efc5239506272b061aee1a0de074
James Benson <you@example.com>
2019-02-08 11:23:26 +0000

    Added package show, gets data from NPMS

--- controllers/packages.js
+++ controllers/packages.js
@@ -1,20 +1,37 @@
 const Package = require('../models/package')
 
+const rp = require('request-promise')
+
 function indexRoute(req, res) {
   Package
     .find()
-    //Populate referenced properties
-    // .populate([{
-    //   path: 'author',
-    //   select: 'name'
-    // },{
-    //   path: 'series',
-    //   select: 'name'
-    // }])
     .then(packages => res.json(packages))
+}
+function showRoute(req, res) {
+  console.log('name',req.params.name)
+  const name = req.params.name
+  var options = {
+    uri: `https://api.npms.io/v2/package/${req.params.name}`,
+    json: true // Automatically parses the JSON string in the response
+  }
+  //
+  rp(options)
+    .then((data)=>{
+      // console.log(data)
+      Package
+        .find({name})
+        .then(_package => {
+          const showPackage = {
+            pepino: {_package},
+            npms: {data}
+
+          }
+          res.json(showPackage)
+        })
+    })
 }
 
 module.exports = {
-  index: indexRoute
-  // show: showRoute
+  index: indexRoute,
+  show: showRoute
 }
