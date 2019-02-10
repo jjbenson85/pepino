4bce829addcd40b5ab8cd5b945639448c49746fa
Siddant <siddantgurung@hotmail.co.uk>
2019-02-10 16:50:15 +0000

    started on styling part

--- src/app.js
+++ src/app.js
@@ -6,9 +6,10 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Header from './components/common/header'
 import PackageShow from './components/packages/packageShow'
 // import BooksIndex from './components/books/BooksIndex'
-// import Home from './components/Home'
+import Home from './components/Home'
 
 
+import 'bulma'
 import './scss/style.scss'
 
 class App extends React.Component {
@@ -20,6 +21,8 @@ class App extends React.Component {
           <Header />
           <Switch>
             <Route path="/packages/:name" component={PackageShow} />
+            <Route path="/" component={Home} />
+
           </Switch>
         </main>
       </BrowserRouter>
---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+//import axios from 'axios'
+
+
+class Home extends React.Component{
+
+  constructor(){
+    super()
+    this.state = {}
+
+  }
+
+  // componentDidMount(){
+  //   this.getPackageData()
+  // }
+
+  render(){
+  //  if(!this.state.package) return null
+
+    return(
+      <h1>Home</h1>
+    )
+  }
+}
+
+export default Home
