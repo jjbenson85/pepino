e46828a62e58581f1bea0d516005f3ee15fc5afa
Begona Fernandez <boadicea1979@yahoo.es>
2019-02-15 10:49:06 +0000

    added fade in to tags and fixed error logs ifo element does not exist

--- src/components/packages/InstalledPackageIndex.js
+++ src/components/packages/InstalledPackageIndex.js
@@ -1,9 +1,6 @@
 import React from 'react'
 
 import axios from 'axios'
-// import Auth from '../../lib/Auth'
-//
-// import SearchBar from '../common/SearchBar'
 import PackageCard from './PackageCard'
 
 class PackageIndex extends React.Component{
@@ -47,10 +44,8 @@ class PackageIndex extends React.Component{
 
   render(){
     this.getUsedPackagesIds()
-    console.log('InstalledPackageIndex state',this.state.packages)
     return(
       <section className='package-index'>
-        {/*<SearchBar url='/api/packages/search' returnData={this.returnData}/>*/}
         <div>
           {this.state.error &&
           <div className="card">
@@ -67,75 +62,6 @@ class PackageIndex extends React.Component{
               userId={this.props.userId}
               getUsedPackagesIds={()=>this.getUsedPackagesIds()}
             />
-            /*<div key={i} className='card'>
-              <div className='card-header'>
-                <div className="media">
-                  <div className="media-left">
-                    {_package.icon &&<div className="">
-                      <div className="comment-image level-item">
-                        <figure className="image" style={{ backgroundImage: `url(${_package.icon})` }} />
-                      </div>
-                    </div>}
-                  </div>
-                  <div className="media-content">
-                    <p className="title is-4">{_package.name}</p>
-                    <p className="subtitle is-6">{_package.version}</p>
-                  </div>
-                </div>
-              </div>
-              <div className='card-content'>
-                <div className="field is-grouped is-grouped-multiline">
-                  <div className="control">
-                    <div className="tags has-addons">
-                      <span className="tag is-dark">version</span>
-                      <span className="tag is-info">{_package.version}</span>
-                    </div>
-                  </div>
-                  {_package.comments && <div className="control">
-                    <div className="tags has-addons">
-                      <span className="tag is-dark">comments</span>
-                      <span className="tag is-success">{_package.comments.length}</span>
-                    </div>
-                  </div>}
-                  {!_package.comments && <div className="control">
-                    <div className="tags has-addons">
-                      <span className="tag is-dark">comments</span>
-                      <span className="tag is-success">0</span>
-                    </div>
-                  </div>}
-                  <div className="control">
-                    <div className="tags has-addons">
-                      <span className="tag is-dark">downloads</span>
-                      <span className="tag is-warning">{Math.round(_package.downloadsCount/1000)}k</span>
-                    </div>
-                  </div>
-                </div>
-                <div className='content'><blockquote className='is-medium'>{_package.description}</blockquote></div>
-                {_package.keywords && <div className="tags level-item " >{_package.keywords.map( (keyword,j)=> <div key={j} className="tag is-primary">{keyword}</div>)}</div>}
-              </div>
-              <div className="card-footer">
-                <div className="card-footer-item buttons has-addons is-fullwidth">
-                  <button
-                    className="button is-success is-outlined "
-                    name="package"
-                    value={_package._id}
-                    onClick={() => this.props.handleAddClick(_package)}
-                    disabled={!Auth.checkAvailability(this.props.userId) || this.getUsedPackagesIds().includes(_package._id)}
-                  >
-                    +Add to project
-                  </button>
-                  <button
-                    className="button is-info is-outlined"
-                    name='viewPackage'
-                    value={_package._id}
-                    onClick={() => this.props.handleViewClick(_package)}
-                  >
-                  View Details
-                  </button>
-                </div>
-
-              </div>
-            </div>*/
           )}
         </div>
       </section>
