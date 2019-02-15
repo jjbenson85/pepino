14499805cb206ad26eb2f4de386df03cde22a99b
Begona Fernandez <boadicea1979@yahoo.es>
2019-02-15 11:33:37 +0000

    fix project scroll issue

--- src/components/projects/TimeStamp.js
+++ src/components/projects/TimeStamp.js
@@ -1,7 +1,7 @@
 import React from 'react'
 const TimeStamp = ({createdAt, updatedAt}) => {
   return (
-    <div className="columns scroll">
+    <div className="columns timeStamp">
       <div className="column">Created at: {createdAt.split('T')[0]} </div>
       <div className="column">Updated at: {updatedAt.split('T')[0]}</div>
     </div>
